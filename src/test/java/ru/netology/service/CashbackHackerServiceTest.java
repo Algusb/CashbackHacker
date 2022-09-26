b033a9a35904421f781c15ae44e6be5e3aaa9244
Aleks Gusev <algusb@mail.ru>
2022-09-26 12:30:59 +0300

    create testng

--- src/test/java/ru/netology/service/CashbackHackerServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackerServiceTest.java
@@ -1,17 +1,47 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-class CashbackHackerServiceTest {
+
+public class CashbackHackerServiceTest {
 
     @Test
-    void remain() {
+    public void testRemain() {
         CashbackHackerService service = new CashbackHackerService();
-        int expected = 200;
-        int actual = service.remain(800);
-        Assertions.assertEquals(expected, actual);
+        int expected = 400;
+        int actual = service.remain(600);
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void testZero() {
+        CashbackHackerService service = new CashbackHackerService();
+        int expected = 1000;
+        int actual = service.remain(0);
+        Assert.assertEquals(actual,expected);
+    }
+    @Test
+    public void testAbove1000() {
+        CashbackHackerService service = new CashbackHackerService();
+        int expected = 800;
+        int actual = service.remain(1200);
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void test1000() {
+        CashbackHackerService service = new CashbackHackerService();
+        int expected = 1000;
+        int actual = service.remain(1000);
+        Assert.assertEquals(actual,expected);
+    }
+    @Test
+    public void testUnderZero() {
+        CashbackHackerService service = new CashbackHackerService();
+        int expected = 1200;
+        int actual = service.remain(-200);
+        Assert.assertEquals(actual,expected);
     }
 }
